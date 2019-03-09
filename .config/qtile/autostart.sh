5db9de3435fd89f7c915b227f39647cbe18e8b1e
Timothy Bissell <tbissell@lossofbreath.com>
2019-03-09 09:40:07 -0500

    tweak the qtile autostart

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-[[ -x "/usr/lib/gsd-xsettings" ]] && /usr/lib/gsd-xsettings &
+# Try to start gnome-session or at least the settings daemon
+([[ -x "/usr/bin/gnome-session" ]] && /usr/bin/gnome-session &) ||
+    ([[ -x "/usr/lib/gsd-xsettings" ]] && /usr/lib/gsd-xsettings &)
 
-[[ -x "/usr/bin/xcompmgr" ]] && /usr/bin/xcompmgr &
+# Run compton or xcompmgr if available
+[[ -x "/usr/bin/xcompmgr" ]] && /usr/bin/xcompmgr -f &
 
+# Run nitrogen if available
 [[ -x "/usr/bin/nitrogen" ]] && /usr/bin/nitrogen --restore
